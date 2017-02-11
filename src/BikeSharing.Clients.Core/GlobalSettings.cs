eaf168552daf1307b04bad12d9ca6b4814e86732
tkitta <tkitta@DESKTOP-7COG73I>
2017-02-11 00:14:53 -0600

    Changes

--- src/BikeSharing.Clients.Core/GlobalSettings.cs
+++ src/BikeSharing.Clients.Core/GlobalSettings.cs
@@ -2,19 +2,19 @@
 {
     public static class GlobalSettings
     {
-        public const string AuthenticationEndpoint = "http://YOUR_PROFILE_SERVICE.azurewebsites.net/";
-        public const string EventsEndpoint = "http://YOUR_EVENTS_SERVICE.azurewebsites.net/";
-        public const string IssuesEndpoint = "http://YOUR_ISSUES_SERVICE.azurewebsites.net/";
-        public const string RidesEndpoint = "http://YOUR_RIDES_SERVICE.azurewebsites.net/";
+        public const string AuthenticationEndpoint = "http://bikesharing-services-profilesqyvqvnoyiaenw.azurewebsites.net/";
+        public const string EventsEndpoint = "http://bikesharing-services-eventsqyvqvnoyiaenw.azurewebsites.net/";
+        public const string IssuesEndpoint = "http://bikesharing-services-feedbackqyvqvnoyiaenw.azurewebsites.net/";
+        public const string RidesEndpoint = "http://bikesharing-services-ridesqyvqvnoyiaenw.azurewebsites.net/";
 
-        public const string OpenWeatherMapAPIKey = "YOUR_WEATHERMAP_API_KEY";
+        public const string OpenWeatherMapAPIKey = "f0944223b33f8e0965afb381ce3f8753";
 
-        public const string HockeyAppAPIKeyForAndroid = "YOUR_HOCKEY_APP_ID";
+        public const string HockeyAppAPIKeyForAndroid = "40b21ba899384cca9171df1f242c4754";
         public const string HockeyAppAPIKeyForiOS = "YOUR_HOCKEY_APP_ID";
 
         public const string SkypeBotAccount = "skype:YOUR_BOT_ID?chat";
 
-        public const string BingMapsAPIKey = "YOUR_BINGMAPS_API_KEY";
+        public const string BingMapsAPIKey = "AjHU86anYGVosiRAZz1ccbrDdeqvCYBbTNBfiMez65zphPJReM-6ateXN8uWINpA";
 
         public static string City => "New York City";
 
---
+++ src/BikeSharing.Tests.Droid/Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Android;
+
+namespace BikeSharing.Tests.Droid
+{
+    [TestFixture]
+    public class Tests
+    {
+        AndroidApp app;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            // TODO: If the Android app being tested is included in the solution then open
+            // the Unit Tests window, right click Test Apps, select Add App Project
+            // and select the app projects that should be tested.
+            app = ConfigureApp
+                .Android
+                // TODO: Update this path to point to your Android app and uncomment the
+                // code if the app is not included in the solution.
+                //.ApkFile ("../../../Android/bin/Debug/UITestsAndroid.apk")
+                .StartApp();
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+
+        [Test]
+        public void NewTest()
+        {
+            app.Tap(x => x.Marked("username"));
+            app.EnterText(x => x.Marked("username"), "scottgu");
+            app.Tap(x => x.Marked("password"));
+            app.EnterText(x => x.Marked("password"), "test");
+            app.Tap(x => x.Marked("signin"));
+            app.Tap(x => x.Id("button1"));
+        }
+
+        [Test]
+        public void NewTest1()
+        {
+            app.Tap(x => x.Marked("username"));
+            app.EnterText(x => x.Marked("username"), "larar");
+            app.Tap(x => x.Marked("password"));
+            app.Tap(x => x.Marked("signin"));
+            app.Screenshot("Tapped on view with class: AppCompatButton marked: signin");
+        }
+    }
+}
+
